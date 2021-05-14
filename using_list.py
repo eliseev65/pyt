821e5ae6594dbb673b8ef71a1b60a4668a2741b8
eliseev65 <vano.eliseev@mail.ru>
2021-05-14 18:04:03 +0300

    dev

--- using_list.py
+++ using_list.py
@@ -13,3 +13,11 @@ print("Now my list to buy is:\n\t", shoplist)
 
 print("I need to sort it.")
 shoplist.sort()
+print("Sortet list is:\n\t", shoplist)
+
+print("First item is", shoplist[0])
+olditem = shoplist[0]
+del shoplist[0]
+print("I bought", olditem)
+print("Now my list is:\n\t",shoplist)
+
---
+++ using_tuple.py
@@ -0,0 +1,8 @@
+zoo = "python", "elephant", "pinguin"
+print("Number of animals is:", len(zoo))
+
+new_zoo = "monkey", "camel", zoo
+print(len(new_zoo))
+print(new_zoo)
+print(new_zoo[2][2])
+print(len(new_zoo) - 1 + len(new_zoo[2]))
